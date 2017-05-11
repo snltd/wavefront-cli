74c5a41946dcbbdc13b4f61f07cd03d0973cf152
Robert Fisher <slackboy@gmail.com>
2017-05-11 14:27:17 +0100

    'agent' is now 'proxy'

---
+++ lib/wavefront-cli/proxy.rb
@@ -0,0 +1,40 @@
+require_relative './base'
+
+module WavefrontCli
+  #
+  # CLI coverage for the v2 'proxy' API.
+  #
+  class Proxy < WavefrontCli::Base
+    include WavefrontCli::Constants
+
+    def do_list
+      @response = :verbose
+      wf.list(options[:offset] || 0, options[:limit] || 100)
+    end
+
+    def do_describe
+      @response = :verbose
+      wf.describe(options[:'<id>'])
+    end
+
+    def do_delete
+      wf.delete(options[:'<id>'])
+    end
+
+    def do_undelete
+      wf.undelete(options[:'<id>'])
+    end
+
+    def humanize_undelete_output(data)
+      puts "undeleted agent #{data['id']}: #{data['name']}."
+    end
+
+    def do_rename
+      wf.rename(options[:'<id>'], options[:'<name>'])
+    end
+
+    def humanize_rename_output(data)
+      puts "renamed #{data['id']} to '#{data['name']}'"
+    end
+  end
+end
