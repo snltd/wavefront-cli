bf8f5c3aff58207500c0c7e2eb0445b3df6ddbc8
Robert Fisher <rob.fisher@bgch.co.uk>
2017-05-23 17:42:02 +0100

    enable all webhook tests

--- spec/wavefront-cli/webhook_spec.rb
+++ spec/wavefront-cli/webhook_spec.rb
@@ -11,7 +11,7 @@ describe "#{word} command" do
   missing_creds(word, ['list', "describe #{id}", "delete #{id}" ])
   list_tests(word)
   cmd_to_call(word, "describe #{id}", path: "/api/v2/#{word}/#{id}")
-  #cmd_to_call(word, "delete #{id}", method: :delete,
-                                    #path:   "/api/v2/#{word}/#{id}")
-  #invalid_ids(word, ["describe #{bad_id}", "delete #{bad_id}"])
+  cmd_to_call(word, "delete #{id}", method: :delete,
+                                    path:   "/api/v2/#{word}/#{id}")
+  invalid_ids(word, ["describe #{bad_id}", "delete #{bad_id}"])
 end
