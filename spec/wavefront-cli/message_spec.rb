3e878a09f079c2425682957eedf0b3c45703b85e
Robert Fisher <rob.fisher@bgch.co.uk>
2017-05-23 18:15:50 +0100

    add message tests

---
+++ spec/wavefront-cli/message_spec.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+id = 'message123'
+bad_id = '(>_<)'
+word = 'message'
+
+require_relative '../spec_helper'
+require_relative "../../lib/wavefront-cli/#{word}"
+
+describe "#{word} command" do
+  missing_creds(word, ['list', "mark #{id}"])
+
+  cmd_to_call(word, 'list',
+              path: "/api/v2/#{word}?limit=100&offset=0&unreadOnly=true")
+  cmd_to_call(word, 'list -L 50',
+              path: "/api/v2/#{word}?limit=50&offset=0&unreadOnly=true")
+  cmd_to_call(word, 'list -L 20 -o 8 -a',
+              path: "/api/v2/#{word}?limit=20&offset=8&unreadOnly=false")
+  cmd_to_call(word, 'list -a -o 60',
+              path: "/api/v2/#{word}?limit=100&offset=60&unreadOnly=false")
+  cmd_to_call(word, "list -a",
+              path: "/api/v2/#{word}?offset=0&limit=100&unreadOnly=false")
+  cmd_to_call(word, "list -L 50 -a",
+              path: "/api/v2/#{word}?offset=0&limit=50&unreadOnly=false")
+  cmd_to_call(word, "mark #{id}",
+              { method: :post, path: "/api/v2/#{word}/#{id}/read" })
+  invalid_ids(word, ["mark #{bad_id}"])
+end
