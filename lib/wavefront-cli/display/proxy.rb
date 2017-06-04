27155df6bcd96e826e96c4d3e28d88eee5107b6a
Robert Fisher <slackboy@gmail.com>
2017-06-05 00:49:15 +0100

    add proxy command

---
+++ lib/wavefront-cli/display/proxy.rb
@@ -0,0 +1,14 @@
+require_relative './base'
+
+module WavefrontDisplay
+
+  # Format human-readable output for proxies.
+  #
+  class Proxy < Base
+
+    def do_describe
+      readable_time(:lastCheckInTime)
+      long_output
+    end
+  end
+end
