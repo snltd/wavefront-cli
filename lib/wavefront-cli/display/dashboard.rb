40b9c320c9ecb071499b3337d8084b097196a29b
Robert Fisher <slackboy@gmail.com>
2017-06-05 00:01:01 +0100

    add dashboard command

---
+++ lib/wavefront-cli/display/dashboard.rb
@@ -0,0 +1,15 @@
+require_relative './base'
+
+module WavefrontDisplay
+
+  # Format human-readable output for dashboards.
+  #
+  class Dashboard < Base
+
+    def do_list
+      long_output [:id, :minutes, :target, :status, :tags, :hostsUsed,
+                   :condition, :displayExpression, :severity,
+                   :additionalInformation]
+    end
+  end
+end
