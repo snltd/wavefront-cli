999224f3dcb6d984da28fbd4bd481a47593f11c8
Robert Fisher <rob.fisher@bgch.co.uk>
2017-06-05 16:38:55 +0100

    source formatter

---
+++ lib/wavefront-cli/display/source.rb
@@ -0,0 +1,18 @@
+require_relative './base'
+
+module WavefrontDisplay
+
+  # Format human-readable output for webhooks.
+  #
+  class Source < Base
+    #def do_list
+      #long_output([:id, :description, :createdEpochMillis,
+                   #:updatedEpochMillis, :updaterId, :creatorId,
+                   #:title])
+    #end
+#
+    def do_list_brief
+      terse_output(:id, :description)
+    end
+  end
+end
