4c0ce3e834c01c9bf70d70f42e2abb75090dbef7
Robert Fisher <slackboy@gmail.com>
2017-07-11 00:46:20 +0100

    fix metric diplsay

--- lib/wavefront-cli/display/metric.rb
+++ lib/wavefront-cli/display/metric.rb
@@ -6,17 +6,16 @@ module WavefrontDisplay
   #
   class Metric < Base
     def do_describe
-      if data.hosts.empty?
+      if data.empty? || data.hosts.empty?
         puts 'No matches.'
         exit
       end
 
-      modified_data = data['hosts'].map do |h, _aggr|
-        { host:        h[:host],
-          last_update: human_time(h[:last_update]) }
+      @data = data['hosts'].map do |h, _aggr|
+        { host: h[:host], last_update: human_time(h[:last_update]) }
       end.sort_by { |h| h[:last_update] }.reverse
 
-      multicolumn(:host, :last_update, modified_data)
+      multicolumn(:host, :last_update)
     end
   end
 end
