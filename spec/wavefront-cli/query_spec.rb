bdcb8f65927d7aa7f4eb2c393a39c41ec0ceaa60
Robert Fisher <services@id264.net>
2019-03-07 11:49:05 +0000

    whitespace

--- spec/wavefront-cli/query_spec.rb
+++ spec/wavefront-cli/query_spec.rb
@@ -89,7 +89,6 @@ describe "#{word} command" do
   cmd_to_call(word, "raw #{s_and_e_opts} -H h1 dev.cli.test",
               path: '/api/v2/chart/raw?metric=dev.cli.test&source=h1' \
                     "&startTime=#{t1}&endTime=#{t2}")
-
 end
 
 describe 'output formatting' do
