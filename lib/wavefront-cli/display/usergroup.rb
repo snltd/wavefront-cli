a74a506cb1a1876f178e836d160f6bb635ca4d9c
Robert Fisher <services@id264.net>
2019-08-26 15:43:18 +0100

    make output quoting more uniform

--- lib/wavefront-cli/display/usergroup.rb
+++ lib/wavefront-cli/display/usergroup.rb
@@ -14,39 +14,45 @@ module WavefrontDisplay
     end
 
     def do_add_user
-      puts format('Added %s to %s.', options[:'<user>'].join(', '),
+      puts format("Added %s to '%s'.", quoted_users,
                   options[:'<id>']).fold(TW, 0)
     end
 
     def do_remove_user
-      puts format("Removed '%s' from %s.", options[:'<user>'].join(', '),
+      puts format("Removed %s from '%s'.", quoted_users,
                   options[:'<id>']).fold(TW, 0)
     end
 
     def do_grant
-      puts format("Granted '%s' permission to %s.",
+      puts format("Granted '%s' permission to '%s'.",
                   options[:'<permission>'], options[:'<id>'])
     end
 
     def do_revoke
-      puts format("Revoked '%s' permission from %s.",
+      puts format("Revoked '%s' permission from '%s'.",
                   options[:'<permission>'], options[:'<id>'])
     end
 
     def do_users
-      if data.users.empty?
-        puts 'No users in group.'
-      else
-        data.users.each { |u| puts u }
-      end
+      puts(if ! data.include?(:users) || data[:users].empty?
+             "No users in group '#{options[:'<id>']}'."
+           else
+             data[:users]
+           end)
     end
 
     def do_permissions
-      if data.permissions.empty?
-        puts 'Group has no permissions.'
-      else
-        data.permissions.each { |u| puts u }
-      end
+      puts(if ! data.include?(:permissions) || data[:permissions].empty?
+            "Group '#{options[:'<id>']}' has no permissions."
+           else
+            data[:permissions]
+           end)
+    end
+
+    private
+
+    def quoted_users
+      options[:'<user>'].map{ |u| "'#{u}'" }.join(', ')
     end
   end
 end
