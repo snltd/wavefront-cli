1f3f6b14e58246a8e2bfd80013b85da2f68bc805
Robert Fisher <services@id264.net>
2019-08-26 10:56:13 +0100

    port to new test framework

--- spec/wavefront-cli/message_spec.rb
+++ spec/wavefront-cli/message_spec.rb
@@ -1,33 +1,75 @@
 #!/usr/bin/env ruby
 
-id = 'CLUSTER::IHjNaHM9'
-bad_id = '(>_<)'
-word = 'message'
+require_relative 'command_base'
+require_relative '../../lib/wavefront-cli/message'
 
-require_relative '../spec_helper'
-require_relative "../../lib/wavefront-cli/#{word}"
+# Ensure 'message' commands produce the correct API calls.
+#
+class MessageEndToEndTest < EndToEndTest
+  def test_list
+    quietly do
+      assert_cmd_gets('list',
+                      '/api/v2/message?limit=100&offset=0&unreadOnly=true')
+      assert_cmd_gets('list -l',
+                      '/api/v2/message?limit=100&offset=0&unreadOnly=true')
+    end
 
-describe "#{word} command" do
-  missing_creds(word, ['list', "mark #{id}"])
-  cmd_noop(word, 'list',
-           ["GET https://metrics.wavefront.com/api/v2/#{word}",
-            offset: 0, limit: 100, unreadOnly: true])
-  cmd_noop(word, "mark #{id}",
-           ["POST https://metrics.wavefront.com/api/v2/#{word}/#{id}/read"])
+    assert_noop('list',
+                'uri: GET https://default.wavefront.com/api/v2/message',
+                'params: ' + {
+                  offset: 0, limit: 100, unreadOnly: true
+                }.to_s)
 
-  cmd_to_call(word, 'list',
-              path: "/api/v2/#{word}?limit=100&offset=0&unreadOnly=true")
-  cmd_to_call(word, 'list -L 50',
-              path: "/api/v2/#{word}?limit=50&offset=0&unreadOnly=true")
-  cmd_to_call(word, 'list -L 20 -o 8 -a',
-              path: "/api/v2/#{word}?limit=20&offset=8&unreadOnly=false")
-  cmd_to_call(word, 'list -a -o 60',
-              path: "/api/v2/#{word}?limit=100&offset=60&unreadOnly=false")
-  cmd_to_call(word, 'list -a',
-              path: "/api/v2/#{word}?offset=0&limit=100&unreadOnly=false")
-  cmd_to_call(word, 'list -L 50 -a',
-              path: "/api/v2/#{word}?offset=0&limit=50&unreadOnly=false")
-  cmd_to_call(word, "mark #{id}",
-              method: :post, path: "/api/v2/#{word}/#{id}/read")
-  invalid_ids(word, ["mark #{bad_id}"])
+    assert_abort_on_missing_creds('list')
+  end
+
+  def test_list_offsets
+    quietly do
+      assert_cmd_gets('list --offset 2 --limit 3',
+                      '/api/v2/message?limit=3&offset=2&unreadOnly=true')
+    end
+  end
+
+  def test_list_all
+    quietly do
+      assert_cmd_gets('list -a',
+                      '/api/v2/message?limit=100&offset=0&unreadOnly=false')
+    end
+  end
+
+  def test_read
+    # TODO: add a proper chained test like we have for
+    # MaintenanceWindow#close, when there's some sample data to work
+    # with. i.e. next time Wavefront send me a message
+    assert_cannot_noop("read #{id}")
+    assert_abort_on_missing_creds("read #{id}")
+    assert_usage('read')
+  end
+
+  def test_mark
+    quietly do
+      assert_cmd_posts("mark #{id}", "/api/v2/message/#{id}/read")
+    end
+
+    assert_noop("mark #{id}",
+                'uri: POST https://default.wavefront.com/api/v2/' \
+                "message/#{id}/read",
+                'body: null')
+    assert_abort_on_missing_creds("mark #{id}")
+    assert_usage('mark')
+  end
+
+  private
+
+  def id
+    'CLUSTER::IHjNaHM9'
+  end
+
+  def invalid_id
+    '(>_<)'
+  end
+
+  def cmd_word
+    'message'
+  end
 end
