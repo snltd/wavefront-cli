03c0f308671bd5b1b51940e5a552c1dbad4e817b
Robert Fisher <services@id264.net>
2021-03-09 23:13:40 +0000

    fix end-to-end tests

--- spec/wavefront-cli/write_spec.rb
+++ spec/wavefront-cli/write_spec.rb
@@ -9,7 +9,7 @@ require_relative '../../lib/wavefront-cli/write'
 class WriteEndToEndTest < EndToEndTest
   def test_write_point_via_api
     out, err = capture_io do
-      assert_cmd_posts('-u api -H tester point test.path 1',
+      assert_cmd_posts("#{api_creds} -u api -H tester point test.path 1",
                       '/report?f=wavefront',
                       'test.path 1.0 source=tester',
                       nil,
@@ -33,7 +33,7 @@ class WriteEndToEndTest < EndToEndTest
     bad_response = { status: { result: 'OK', message: '', code: 200 },
                      items: [] }.to_json
 
-      assert_cmd_posts('-u api -H tester point test.path 1',
+      assert_cmd_posts("#{api_creds} -u api -H tester point test.path 1",
                       '/report?f=wavefront',
                       'test.path 1.0 source=tester',
                       bad_response,
@@ -60,13 +60,73 @@ class WriteEndToEndTest < EndToEndTest
     assert_usage('create myname')
   end
 
+  def test_no_config_no_envvars_proxy_write
+    skip if have_config?
+
+    blank_envvars
+    wf = WavefrontCliController
+
+    out, err = capture_io do
+      assert_raises(SystemExit) { wf.new(%w[write point test 1]) }
+    end
+
+    assert_empty(err)
+    assert_match(/Credentials must contain proxy address/, out)
+    assert_match(/You may also run 'wf config setup'/, out)
+  end
+
+  def test_no_config_no_envvars_api_write
+    skip if have_config?
+
+    blank_envvars
+    wf = WavefrontCliController
+
+    out, err = capture_io do
+      assert_raises(SystemExit) { wf.new(%w[write point -u api test 1]) }
+    end
+
+    assert_empty(err)
+    assert_match(/Credentials must contain api token/, out)
+    assert_match(/You may also run 'wf config setup'/, out)
+  end
+
+  def test_no_config_no_envvars_local_socket_write
+    skip if have_config?
+
+    blank_envvars
+    wf = WavefrontCliController
+
+    out, err = capture_io do
+      assert_raises(SystemExit) { wf.new(%w[write point -u unix test 1]) }
+    end
+
+    assert_empty(err)
+    assert_match(/Credentials must contain socket file path/, out)
+    assert_match(/You may also run 'wf config setup'/, out)
+  end
+
+  def test_no_config_no_envvars_http_proxy_write
+    skip if have_config?
+
+    blank_envvars
+    wf = WavefrontCliController
+
+    out, err = capture_io do
+      assert_raises(SystemExit) { wf.new(%w[write point -u http test 1]) }
+    end
+
+    assert_empty(err)
+    assert_match(/Credentials must contain proxy address/, out)
+    assert_match(/You may also run 'wf config setup'/, out)
+  end
+
   private
+
+  def api_creds
+    { endpoint: 'test.wavefront.com', token: 'ABCDE-12345' }
+  end
 
   def cmd_word
     'write'
-  end
-
-  def api_path
-    'extlink'
   end
 end
